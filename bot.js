46268cd8a6fa23ae47b0fdfee45264c3feccee61
Mohamedamine2005 <47900954+Mohamedamine2005@users.noreply.github.com>
2019-03-10 10:09:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,7 +139,7 @@ client.on('message', message => {
   if (!message.guild) return;
 
   // If the message content starts with "%kick"
-  if (message.content.startsWith('!kick')) {
+  if (message.content.startsWith('%kick')) {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
     const user = message.mentions.users.first();
